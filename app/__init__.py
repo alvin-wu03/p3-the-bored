d90864003490f4a7958decd8f3c43c0f77307d23
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2021-04-23 17:00:41 -0400

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,8 @@
+# Team TheBored (Alvin Wu, William Yin, Jeffrey Huang)
+# SoftDev
+# P3 -- ArRESTed Development, JuSt in Time
+# 2021-04-29
+
 from flask import Flask,session            #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request, redirect           #facilitate form submission
@@ -10,14 +15,14 @@ app.secret_key = os.urandom(24)
 
 @app.route("/") #testing purposes
 def renderlogin():
-    return render_template("edit_review.html", oldtitle = "TITLE", oldreviewtext = "TEXT")   
+    return render_template("edit_review.html", oldtitle = "TITLE", oldreviewtext = "TEXT")
 
 @app.route("/search") #tested search function to see if category and query could be accessed, the answer is yes
-def search(): 
+def search():
     print("THIS IS THE CATEGORY: " + request.args['category'])
     print("THIS IS THE SEARCH QUERY: " + request.args['searchquery'])
     return True
 
 if __name__ == "__main__": #false if this file imported as module
-    app.debug = True 
+    app.debug = True
     app.run()
