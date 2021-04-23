9d36154d076a4949c6c0ba3332b95b7993592b8a
Jeffrey Huang <71650487+Jeffrey-Huang11@users.noreply.github.com>
2021-04-23 17:14:05 -0400

    added and modified login and init.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@
 # SoftDev
 # P3 -- ArRESTed Development, JuSt in Time
 # 2021-04-29
-
 from flask import Flask,session            #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request, redirect           #facilitate form submission
@@ -10,8 +9,30 @@ from datetime import datetime
 import os
 import sqlite3   #enable control of an sqlite database
 
+#Create db for user and story information
+db = sqlite3.connect("p0database.db")
+c = db.cursor()
+#c.execute("DROP TABLE IF EXISTS stories") #for changing columns
+#c.execute("DROP TABLE IF EXISTS users") #for changing columns
+c.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text, biography text, reviews text)""")
+c.execute("""CREATE TABLE IF NOT EXISTS reviews (id INTEGER PRIMARY KEY, product text, rating nteger, review text, type text)""")
+db.commit()
+
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+#Checks if user is in session
+@app.route("/", methods = ['GET', 'POST']) #methods=['GET', 'POST']
+def disp_loginpage():
+    if "user" in session:
+        return render_template('home.html', user = session["user"])
+    else:
+        return render_template('login.html')
+
+#Routes user to registration page
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    return render_template('signup.html')
 
 @app.route("/") #testing purposes
 def renderlogin():
