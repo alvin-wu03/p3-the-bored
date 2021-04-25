0c67cc36d05fc927a043d5ad3ba0f676e7a244c8
William-Yin123 <william.yin.6495@gmail.com>
2021-04-25 09:46:34 -0400

    redid user authentication

--- app/__init__.py
+++ app/__init__.py
@@ -1,126 +1,101 @@
-# Team TheBored (Alvin Wu, William Yin, Jeffrey Huang)
+# Team TheBored -- Alvin Wu, William Yin, Jeffrey Huang
 # SoftDev
 # P3 -- ArRESTed Development, JuSt in Time
 # 2021-04-29
-from flask import Flask,session            #facilitate flask webserving
-from flask import render_template   #facilitate jinja templating
-from flask import request, redirect           #facilitate form submission
-from datetime import datetime
+
+
+from flask import Flask, render_template, request, session, url_for, redirect, abort
+from flask_bcrypt import Bcrypt
+import bcrypt
 import os
-import sqlite3   #enable control of an sqlite database
+import time
+import sqlite3
+import utils
 
-#Create db for user and story information
-db = sqlite3.connect("p3database.db")
-c = db.cursor()
-#c.execute("DROP TABLE IF EXISTS reviews") #for changing columns
-c.execute("DROP TABLE IF EXISTS users") #for changing columns
-c.execute("""CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text, bibliography text, reviews text)""")
-c.execute("""CREATE TABLE IF NOT EXISTS reviews (id INTEGER PRIMARY KEY, product text, rating integer, review text, type text)""")
-db.commit()
 
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
+APP_NAME = "The Board"
+app = Flask(APP_NAME)
+bcrypt = Bcrypt(app)
+app.secret_key = os.urandom(32)
+DB_FILE = "the_board.db"
 
-#Checks if user is in session
-@app.route("/", methods = ['GET', 'POST']) #methods=['GET', 'POST']
-def disp_loginpage():
-    if "user" in session:
-        return render_template('home.html', user = session["user"])
-    else:
-        return render_template('login.html')
 
-#Routes user to registration page
-@app.route("/register", methods = ['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template('register.html')
 
-#Registration for new user, stores user info into users db
-@app.route("/register_auth", methods = ['GET', 'POST'])
-def registerConfirming():
-    db = sqlite3.connect("p3database.db")
-    c1 = db.cursor()
+    if request.method == "GET":
+        return render_template("register.html")
 
-    #gets all the data from the register.html form to check if they exist/match
-    u = request.form['new_username']
-    p = request.form['new_password_1']
-    p1 = request.form['new_password_2']
-    b = request.form['new_bibliography']
+    if not request.form.get("username") or not request.form.get("password") or not request.form.get("confirm password") or not request.form.get("biography"):
+        return render_template("register.html", warning="Please fill out all fields.")
+    elif len(request.form.get("password")) < 4:
+        return render_template("register.html", warning="Password must be at least 4 characters long.")
+    elif request.form.get("password") != request.form.get("confirm password"):
+        return render_template("register.html", warning="Passwords do not match.")
 
-    #Gets a list of all the registered usernames to check later on
-    usernames_list = []
-    for x in c1.execute("SELECT username FROM users;"):
-        usernames_list.append(x[0])
+    db = sqlite3.connect(DB_FILE)
+    cursor = db.cursor()
+    cursor.execute("select * from users where username = ?", (request.form.get("username"),))
+    user = cursor.fetchone()
+    if user:
+        db.close()
+        return render_template("register.html", warning="Username is already taken.")
 
-    if len(u.strip()) == 0:
-        return render_template('register.html', error_type = "Please enter valid username, try again")
-    elif len(p.strip()) == 0:
-        return render_template('register.html', error_type = "Please enter valid password, try again")
-    elif len(b.strip()) == 0:
-        return render_template('register.html', error_type = "Please enter valid bibliography, try again")
-    #Checks if the username exists
-    elif u in usernames_list:
-        return render_template('register.html', error_type = "Username already exists, try again")
-    #Checks if the passwords match
-    elif p != p1:
-        return render_template('register.html', error_type = "Passwords do not match, try again")
-    #If both pass, it adds the newly registered user and directs the user to the login page
-    else:
-        c1.execute("INSERT INTO users (username, password, bibliography) VALUES (?, ?, ?)", (u, p, b))
-        db.commit()
-        return render_template("login.html", error_type = "Please login with your new account")
+    password_hash = bcrypt.generate_password_hash(request.form.get("password"))
+    cursor.execute("insert into users (username, password, biography) values (?, ?, ?)", (request.form.get("username"), password_hash, request.form.get("biography")))
+    cursor.execute("select * from users where username = ?", (request.form.get("username"),))
+    user = cursor.fetchone()
+    db.commit()
+    db.close()
 
-#Checks credentials of login attempt
-@app.route("/auth", methods = ['GET', 'POST']) # methods=['GET', 'POST']
-def welcome():
-    db = sqlite3.connect("p3database.db")
-    c2 = db.cursor()
-    username = request.form['username']
-    password = request.form['password']
-
-    u_list = []
-    for x in c2.execute("SELECT username FROM users"):
-        for y in x:
-            u_list.append(y)
-    p_list = []
-    for a in c2.execute("SELECT password FROM users"):
-        for b in a:
-            p_list.append(b)
-
-#    usersContributions = []      ; will be used for userReviews later
-#    if username in u_list:
-#        user_index = u_list.index(username)
-#        for x in c2.execute("SELECT contributions FROM users"):
-#            usersContributions.append(x[0])
-#        user_conts = usersContributions[user_index].split("~")
-#
-#        if (len(user_conts) >= 1):
-#            user_conts.pop()
-
-    if username in u_list and password in p_list:
-        session["user"] = username
-        return render_template('home.html', user = username, message = "Your Login Has Been Successful! \(^-^)/")
-    else:
-        return render_template('login.html', error_type = "Invalid login attempt, please try again.")
+    session["user_id"] = user[0]
+    session["username"] = user[1]
+    return redirect(url_for("index"))
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "GET":
+        if session.get("user_id"):
+            return redirect(url_for("index"))
+        return render_template("login.html")
+
+    if not request.form.get("username") or not request.form.get("password"):
+        return render_template("login.html", warning="Please fill out all fields.")
+
+    db = sqlite3.connect(DB_FILE)
+    cursor = db.cursor()
+    cursor.execute("select * from users where username = ?", (request.form.get("username"),))
+    user = cursor.fetchone()
+    db.close()
+    if not user or not bcrypt.check_password_hash(user[2], request.form.get("password")):
+        return render_template("login.html", warning="Incorrect username or password.")
+
+    session["user_id"] = user[0]
+    session["username"] = user[1]
+    return redirect(url_for("index"))
 
 
-@app.route("/") #testing purposes
-def renderlogin():
-    return render_template("edit_review.html", oldtitle = "TITLE", oldreviewtext = "TEXT")
-
-@app.route("/search") #tested search function to see if category and query could be accessed, the answer is yes
-def search():
-    print("THIS IS THE CATEGORY: " + request.args['category'])
-    print("THIS IS THE SEARCH QUERY: " + request.args['searchquery'])
-    return True
-
-#Displays login page and removes user from session
-@app.route("/logout", methods = ['GET', 'POST'])
+@app.route("/logout")
 def logout():
-    session.pop("user", None) #removes the session
-    return render_template('login.html')
+    if not session.get("user_id") or not session.get("username"):
+        return redirect(url_for("login"))
 
-if __name__ == "__main__": #false if this file imported as module
+    session.pop("user_id")
+    session.pop("username")
+    return redirect(url_for("login"))
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    if not session.get("user_id") or not session.get("username"):
+        return redirect(url_for("login"))
+
+    
+
+
+if __name__ == "__main__":
     app.debug = True
     app.run()
---
+++ app/utils.py
@@ -0,0 +1,48 @@
+import sqlite3
+import time
+import datetime
+
+
+def select_entries_by_author(db_file, author_id=None, exclude=False):
+
+    db = sqlite3.connect(db_file)
+    cursor = db.cursor()
+    if author_id:
+        if exclude:
+            cursor.execute("select * from entries where author_id != ?", (author_id,))
+        else:
+            cursor.execute("select * from entries where author_id = ?", (author_id,))
+    else:
+        cursor.execute("select * from entries")
+    entries_tuples = cursor.fetchall()
+
+    entries = []
+    for tuple in entries_tuples:
+        cursor.execute("select username from users where id = ?", (tuple[1],))
+        author_name = cursor.fetchone()[0]
+        date = datetime.datetime.fromtimestamp(int(tuple[3]))
+        entry = {"title": tuple[4], "body": tuple[2], "author": author_name, "date": date, "author_id": tuple[1], "id": tuple[0]}
+        entries.append(entry)
+    db.close()
+
+    def sort_by_date(entry):
+        return entry["date"]
+    entries_sorted = sorted(entries, key=sort_by_date, reverse=True)
+
+    return entries_sorted
+
+
+def verify_user_owns_entry(db_file, entry_id, user_id):
+
+    db = sqlite3.connect(db_file)
+    cursor = db.cursor()
+    cursor.execute("select * from entries where id = ?", (entry_id,))
+    entry = cursor.fetchone()
+    if not entry:
+        db.close()
+        return (404, None)
+    elif entry[1] != user_id:
+        db.close()
+        return (403, None)
+
+    return (0, entry)
